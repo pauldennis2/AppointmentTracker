266cd615d7f170bb200cceb05a67cc9f154eb84e
pauldennis2 <pauldennis.misc@gmail.com>
2018-03-06 16:03:48 -0500

    previous unsaved changes

--- src/main/java/com/pauldennis/AppointmentTracker/AppointmentDatabase.java
+++ src/main/java/com/pauldennis/AppointmentTracker/AppointmentDatabase.java
@@ -1,5 +1,54 @@
 package com.pauldennis.AppointmentTracker;
 
-public class AppointmentDatabase {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.h2.tools.Server;
+
+public class AppointmentDatabase {
+	
+	public final static String DB_URL = "jdbc:h2:file:~/test";
+	
+	private Connection conn;
+	
+	public void init() throws SQLException {
+        Server.createWebServer().start();
+        conn = DriverManager.getConnection(DB_URL);
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS appointments (id IDENTITY, date VARCHAR, time VARCHAR, description VARCHAR)");
+    }
+	
+	public void addAppointment(Appointment appointment) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO appointments VALUES (?, ?, ?);");
+        stmt.setString(1, appointment.getDate());
+        stmt.setString(2, appointment.getTime());
+        stmt.setString(3, appointment.getDescription());
+        stmt.execute();
+    }
+	
+	public List<Appointment> getAllAppointments () throws SQLException {
+		List<Appointment> appointments = new ArrayList<>();
+        Statement stmt = conn.createStatement();
+        ResultSet results = stmt.executeQuery("SELECT * FROM appointments");
+        while (results.next()) {
+            String date = results.getString("date");
+            String time = results.getString("time");
+            String description = results.getString("description");
+            appointments.add(new Appointment(date, time, description));
+        }
+        return appointments;
+	}
+	
+	public List<Appointment> getAppointmentsByName (String name) throws SQLException {
+		return getAllAppointments().stream()
+			.filter(appointment -> appointment.getDescription().contains(name))
+			.collect(Collectors.toList());
+	}
 }
--- src/main/java/com/pauldennis/AppointmentTracker/AppointmentTrackerController.java
+++ src/main/java/com/pauldennis/AppointmentTracker/AppointmentTrackerController.java
@@ -1,5 +1,6 @@
 package com.pauldennis.AppointmentTracker;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -12,17 +13,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AppointmentTrackerController {
 	
+	AppointmentDatabase appointmentDatabase;
+	
 	@RequestMapping (path = "/", method = RequestMethod.GET)
-	public String home (HttpSession session, Model model) {
-		//Temporary: storing the appointments in the session instead of the db
-		List<Appointment> appointments = (List<Appointment>) session.getAttribute("appointments");
+	public String home (Model model) throws SQLException {
+		if (appointmentDatabase == null) {
+			appointmentDatabase = new AppointmentDatabase();
+			appointmentDatabase.init();
+		}
+		List<Appointment> appointments = appointmentDatabase.getAllAppointments();
 		model.addAttribute("appointments", appointments);
 		System.out.println("At the home endpoint");
 		return "home";
 	}
 	
 	@RequestMapping (path = "/add-appointment", method = RequestMethod.POST)
-	public String addAppointment () {
+	public String addAppointment (String date, String time, String description) throws SQLException {
+		appointmentDatabase.addAppointment(new Appointment(date, time, description));
+		return "home";
+	}
+	
+	@RequestMapping (path = "/search", method = RequestMethod.GET)
+	public String searchAppointments (Model model, String search) throws SQLException {
+		List<Appointment> appointments = appointmentDatabase.getAppointmentsByName(search);
+		model.addAttribute("appointments", appointments);
 		return "home";
 	}
 }
